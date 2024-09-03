b59acbcd31f4a1dabb11489b70485f5011cb0c10
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-09-02 21:05:10 -0500

    Add files via upload

---
+++ f_strings.py
@@ -0,0 +1,16 @@
+kg_value_1 = 69.8
+kg_value_2 = 79.9
+kg_value_3 = 45.3
+kg_value_4 = 108.6
+
+Pounds = kg * 2.20462
+
+pounds_1 = kg_value_1 / conversion_factor
+pounds_2 = kg_value_2 / conversion_factor
+pounds_3 = kg_value_3 / conversion_factor
+pounds_4 = kg_value_4 / conversion_factor
+
+print(f"{kg_value_1} kilograms is equal to {pounds_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_2:.2f} pounds.")
+print(f"{kg_value_3} kilograms is equal to {pounds_3:.2f} pounds.")
+print(f"{kg_value_4} kilograms is equal to {pounds_4:.2f} pounds.")
